8552bc9b74ab13b75412866be747f9201a2aa986
novusthedev <hunterake27@gmail.com>
2024-04-14 19:23:02 -0500

    UI update

--- static/js/media.js
+++ static/js/media.js
@@ -1689,7 +1689,7 @@
                     title: e.name
                 }, a.createElement("span", null, e.name))), o.PageStore.get("config-media-item").displayPublishDate && e.published ? a.createElement("span", {
                     className: "author-banner-date"
-                }, e.author_name + " | ", (0,
+                }, e.author.username + " | ", (0,
                 m.publishedOnDate)(new Date(e.published))) : null))
             }
             function ce(e) {
