e87cace5baaee1ad587bcb7990071f093fcfd490
novusthedev <hunterake27@gmail.com>
2024-05-04 16:07:28 -0500

    Idk still

--- static/js/debug.js
+++ static/js/debug.js
@@ -1,20 +1,20 @@
 // Web Browser
-document.querySelector('#Browser').innerText = toString(navigator.appName);
+document.querySelector('Browser').innerText = toString(navigator.appName);
 
 // Opewrating System
-document.querySelector('#OS').innerText = toString(navigator.platform);
+document.querySelector('OS').innerText = toString(navigator.platform);
 
 // User Agent
-document.querySelector('#UserAgent').innerText = toString(navigator.platform);
+document.querySelector('UserAgent').innerText = toString(navigator.platform);
 
 // Other Options
-document.querySelector('#Cookies').innerText = toString(navigator.cookieEnabled);
-document.querySelector('#Media').innerText = toString(navigator.mediaCapabilities);
+document.querySelector('Cookies').innerText = toString(navigator.cookieEnabled);
+document.querySelector('Media').innerText = toString(navigator.mediaCapabilities);
 
 // Server
 import { spawn } from 'child_process';
 var command = spawn('cat /proc/version');
 
 command.uname.on('kernel', function(kernel) {
-    document.querySelector('#ServerOS').innerText = toString(kernel);
+    document.querySelector('ServerOS').innerText = toString(kernel);
 });
