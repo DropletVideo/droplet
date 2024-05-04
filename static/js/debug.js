19d6d527f9af3117cf7a063c363b4f983ad7a0e7
novusthedev <hunterake27@gmail.com>
2024-05-04 15:57:59 -0500

    Something that MIGHT work

--- static/js/debug.js
+++ static/js/debug.js
@@ -1,20 +1,20 @@
 // Web Browser
-document.getElementById("Browser").innerText = toString(navigator.appName + navigator.appVersion);
+document.getElementById("Browser").textContent = toString(navigator.appName + navigator.appVersion);
 
 // Opewrating System
-document.getElementById("OS").innerText = toString(navigator.platform);
+document.getElementById("OS").textContent = toString(navigator.platform);
 
 // User Agent
-document.getElementById("UserAgent").innerText = toString(navigator.platform);
+document.getElementById("UserAgent").textContent = toString(navigator.platform);
 
 // Other Options
-document.getElementById("Cookies").innerText = toString(navigator.cookieEnabled);
-document.getElementById("Media").innerText = toString(navigator.mediaCapabilities);
+document.getElementById("Cookies").textContent = toString(navigator.cookieEnabled);
+document.getElementById("Media").textContent = toString(navigator.mediaCapabilities);
 
 // Server
 import { spawn } from 'child_process';
 var command = spawn('cat /proc/version');
 
 command.uname.on('kernel', function(kernel) {
-    document.getElementById("ServerOS").innerText = toString(kernel);
+    document.getElementById("ServerOS").textContent = toString(kernel);
 });
