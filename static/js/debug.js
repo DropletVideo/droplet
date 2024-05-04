78e666bebf15726dbd6b99182f6fe60bce77acec
novusthedev <hunterake27@gmail.com>
2024-05-04 15:23:13 -0500

    Add debug page

---
+++ static/js/debug.js
@@ -0,0 +1,20 @@
+// Web Browser
+document.getElementById("p1").innerHTML = toString(navigator.appName + navigator.appVersion);
+
+// Opewrating System
+document.getElementById("p2").innerHTML = toString(navigator.platform);
+
+// User Agent
+document.getElementById("p3").innerHTML = toString(navigator.platform);
+
+// Other Options
+document.getElementById("p4").innerHTML = toString(navigator.cookieEnabled);
+document.getElementById("p5").innerHTML = toString(navigator.mediaCapabilities);
+
+// Server
+import { spawn } from 'child_process';
+var command = spawn('cat /proc/version');
+
+command.uname.on('data', function(kernel) {
+    document.getElementById("p6").innerHTML = toString(kernel);
+});
