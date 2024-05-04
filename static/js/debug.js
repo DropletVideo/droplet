53ccf2091fa6be70d8391890551820bc07b3ad7e
novusthedev <hunterake27@gmail.com>
2024-05-04 15:49:07 -0500

    Fix!!!

--- static/js/debug.js
+++ static/js/debug.js
@@ -1,12 +1,20 @@
 // Web Browser
-document.getElementById("p1").innerHTML = toString(navigator.appName + navigator.appVersion);
+document.getElementById("Browser").innerHTML = toString(navigator.appName + navigator.appVersion);
 
 // Opewrating System
-document.getElementById("p2").innerHTML = toString(navigator.platform);
+document.getElementById("OS").innerHTML = toString(navigator.platform);
 
 // User Agent
-document.getElementById("p3").innerHTML = toString(navigator.platform);
+document.getElementById("UserAgent").innerHTML = toString(navigator.platform);
 
 // Other Options
-document.getElementById("p4").innerHTML = toString(navigator.cookieEnabled);
-document.getElementById("p5").innerHTML = toString(navigator.mediaCapabilities);
+document.getElementById("Cookies").innerHTML = toString(navigator.cookieEnabled);
+document.getElementById("Media").innerHTML = toString(navigator.mediaCapabilities);
+
+// Server
+import { spawn } from 'child_process';
+var command = spawn('cat /proc/version');
+
+command.uname.on('kernel', function(kernel) {
+    document.getElementById("ServerOS").innerHTML = toString(kernel);
+});
